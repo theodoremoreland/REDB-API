d9ee77d029f01b1896c616a7d2c10fd9cd19a4de
Theodore Moreland <theodoremoreland@yahoo.com>
2020-11-08 05:45:51 -0600

    Removed comment and print statement

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,11 +3,8 @@ import sqlalchemy
 import databases
 
 # Custom
-from app.config import DATABASE_URL # DB connection information in local gitignored file
+from app.config import DATABASE_URL
 from app.models.models import *
-
-print(f"_________________________________________________________________{DATABASE_URL}__________________________________________")
-
 
 database = databases.Database(DATABASE_URL)
 engine = sqlalchemy.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
